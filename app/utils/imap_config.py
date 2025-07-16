fb4ef947ef52e6d26dbb6d380fad467ac98b9ff3
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-07-16 21:48:14 +0500

    Add Imap Config File to recieve mails

---
+++ app/utils/imap_config.py
@@ -0,0 +1,8 @@
+# app\utils\imap_config.py
+# Receiving Emails
+
+IMAP_CONFIG = {
+    "IMAP_SERVER": "imap.gmail.com",
+    "EMAIL": "zakaulhassan6717@gmail.com",
+    "PASSWORD": "vwjhyjyhhbaqhsyl"
+}
