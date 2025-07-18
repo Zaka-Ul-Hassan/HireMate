7a9162cab0247c8b2d9743412c8d7f7e3ae4ec47
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-07-18 15:09:49 +0500

    Add schema for InboxEmail

--- app/schemas/email/email_schema.py
+++ app/schemas/email/email_schema.py
@@ -1,9 +1,23 @@
 # app\schemas\email\email_schema.py
 
 from pydantic import BaseModel, EmailStr
-from typing import List
+from typing import List, Optional
+from datetime import datetime
 
 class EmailSchema(BaseModel):
     to : List[EmailStr] # list of emails
     subject : str
     body: str
+
+
+class InboxEmail(BaseModel):
+    message_id:str
+    sender: EmailStr
+    subject: Optional[str]
+    date: Optional[datetime] = None
+    text: Optional[str] = None
+    html: Optional[str] = None
+
+
+    class Config:
+        orm_mode = True
