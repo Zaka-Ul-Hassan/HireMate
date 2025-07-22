56d9f5df1244c4184a9010ae508695429ffbb56b
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-07-22 16:46:10 +0500

    Add Resume model to iniatew the table

--- init_db.py
+++ init_db.py
@@ -6,10 +6,13 @@ from app.db import engine
 from app.models.user.role  import Role
 from app.models.user.user_role import UserRole
 from app.models.email.email_model import Email
+from app.models.resume.resume_model import Resume
 
 
 def create_table():
     Base.metadata.create_all(bind=engine)
+    print(Base.metadata.tables.keys())
+
     print("Table created successfully.")
 
 if __name__ == "__main__":
