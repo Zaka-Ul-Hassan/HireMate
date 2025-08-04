e554c1c54816472ca256414fcf7ee93506fb03ff
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-04 18:13:31 +0500

    Get current user in resume route to send ut to resume service for getting logged in user id

--- app/routes/resume/resume_processing_route.py
+++ app/routes/resume/resume_processing_route.py
@@ -4,8 +4,10 @@ from fastapi import APIRouter,File,UploadFile,Depends
 from fastapi.responses import JSONResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.models.user.user import User
 from app.services.resume.resume_parser_service import extract_text_from_pdf,extract_text_from_docx
 from app.services.resume.resume_processing_service import extract_fields_and_store
+from app.services.authentication.auth_service import get_current_user
 
 from app.db import get_db
 
@@ -31,9 +33,9 @@ async def docx_to_text(file: UploadFile = File(...)):
     
     
 @router.post("/store-process-resume")
-def store_resume(file:UploadFile, db:AsyncSession = Depends(get_db)):
+def store_resume(file:UploadFile, db:AsyncSession = Depends(get_db), user: User = Depends(get_current_user)):
     try:
-        result =  extract_fields_and_store(file,db)
+        result =  extract_fields_and_store(file,db,user)
         return {"result": result}
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": f"{str(e)}"})
