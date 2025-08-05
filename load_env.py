f0dbaf1f98728c949fd21f170822d0352e02c836
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-05 18:21:59 +0500

    Update load_dotenv by adding api keys for google search and and google search engine id cse id

--- load_env.py
+++ load_env.py
@@ -5,3 +5,10 @@ import os
 
 load_dotenv()
 cohere_api_key = os.getenv("COHERE_API_KEY")
+
+google_search_api_key = os.getenv("GOOGLE_SEARCH_API_KEY")
+google_search_cse_id = os.getenv("GOOGLE_SEARCH_CSE_ID")
+
+print(cohere_api_key)
+print(google_search_api_key)
+print(google_search_cse_id)
