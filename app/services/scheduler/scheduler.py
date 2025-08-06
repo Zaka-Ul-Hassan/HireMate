fc215e889d14d3ce61558156c08b3bd845c714a4
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-06 21:09:16 +0500

    Update scheduler backend job by adding the fetch jobs service func

--- app/services/scheduler/scheduler.py
+++ app/services/scheduler/scheduler.py
@@ -4,21 +4,37 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 import atexit
 
-from app.services.email.email_service import fetch_all_emails
+from app.db import SessionLocal
+from app.models.resume.resume_model import Resume
 from app.services.email.email_service import store_emails_in_db
+from app.services.job.job_scanner_service import fetch_jobs_from_api
+
+def combined_job():
+    db = SessionLocal()
+    try:
+        store_emails_in_db()
+
+        resumes = db.query(Resume).all()
+        for resume in resumes:
+            fetch_jobs_from_api(db=db, resume_id=resume.Id)
+    except Exception as e:
+        print(f"Scheduler Job error: {e}")
+    finally:
+        db.close()
 
 def start_email_scheduler():
     scheduler = BackgroundScheduler()
 
     scheduler.add_job(
-        func=store_emails_in_db,
+        func=combined_job,
         trigger=IntervalTrigger(minutes=10),
         id='email_store_job',
         name='Fetch Emails Every 10 Minutes',
         replace_existing=True
     )
-
+    print("start")
     scheduler.start()
+
 
     #scheduler shuts down on app exit
     atexit.register(lambda: scheduler.shutdown())
