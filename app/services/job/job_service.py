597228ce4e549edc780109d2206b70bda10bb781
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-07 20:47:15 +0500

    Remove comments

--- app/services/job/job_service.py
+++ app/services/job/job_service.py
@@ -5,17 +5,6 @@ import requests
 
 from app.models.resume.resume_model import Resume
 
-
-# from fastapi import FastAPI
-# from serpapi import GoogleSearch
-# from typing import List
-# import os
-# pip install fastapi uvicorn beautifulsoup4 requests google-search-results
-# 
-# app = FastAPI()
-
-# SERP_API_KEY = "YOUR_SERPAPI_KEY"  # Replace this with your actual key
-
 def get_job_recommendation(db:Session, resume_id: int):
     resume = db.query(Resume).filter(Resume.Id == resume_id).first()
     if not resume:
