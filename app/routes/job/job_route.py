e8ce06169f525093d406a31bbce5d65e59873c05
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-07 17:51:55 +0500

    Update the route for /api/recommend-jobs/recommend/jobs

--- app/routes/job/job_route.py
+++ app/routes/job/job_route.py
@@ -1,14 +1,19 @@
 # app\routes\job\job_route.py
 
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
+
 from app.db import get_db
+from app.models.user.user import User
+from app.models.resume.resume_model import Resume
+from app.services.authentication.auth_service import get_current_user
 from app.services.job.job_service import get_job_recommendation
 from app.services.job.job_scanner_service import fetch_jobs_from_api
 
 router = APIRouter()
 
-@router.get("/recommend/{resume_id}")
+@router.get("ai/recommend/{resume_id}")
 def recommend_jobs(resume_id: int, db:Session = Depends(get_db)):
     try:
         result = get_job_recommendation(db,resume_id)
@@ -17,12 +22,22 @@ def recommend_jobs(resume_id: int, db:Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail=str(e))
     
 
-@router.post("/recommend/{resume_id}")
-async def search_jobs(resume_id: int, page: int = 2, db: Session = Depends(get_db)):
-    try:
-        result = fetch_jobs_from_api(db, resume_id, page)
-        if "error" in result:
-            raise HTTPException(status_code=404, detail=result["error"])
-        return {"status": "success", "jobs": result}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+@router.post("/api/recommend-jobs/recommend/jobs")
+async def recommend_jobs(
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+    page: int = 2,
+):
+    resume = db.query(Resume).filter(Resume.UserId == current_user.Id).first()
+
+    if not resume:
+        return JSONResponse(
+            status_code=400,
+            content={"detail": "Resume not found"}
+        )
+
+    jobs = fetch_jobs_from_api(db=db, resume_id=resume.Id, page=page)
+
+    return {"jobs": jobs}
+
+
