987d55b0f0ab846cac95ffebdac6db5517881f1e
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-07 20:32:40 +0500

    Update js for toggle icon when user enter password it is with slash and when user click icon password is shown and slash is removed

--- frontend/static/js/shared/password-toggle.js
+++ frontend/static/js/shared/password-toggle.js
@@ -6,11 +6,11 @@ function togglePassword() {
 
   if (password.type === "password") {
     password.type = "text";
-    icon.classList.remove("fa-eye");
-    icon.classList.add("fa-eye-slash");
-  } else {
-    password.type = "password";
     icon.classList.remove("fa-eye-slash");
     icon.classList.add("fa-eye");
+  } else {
+    password.type = "password";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
   }
 }
