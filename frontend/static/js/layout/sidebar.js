6ba79562948737d9657c2cc77a643373bd2b31a0
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-08 18:28:17 +0500

    remove logout button event lister js from sidebar.js

--- frontend/static/js/layout/sidebar.js
+++ frontend/static/js/layout/sidebar.js
@@ -3,7 +3,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   
     const findJobBtn = document.getElementById("findJobButton");
-    console.log(findJobBtn);
     if (findJobBtn) {
         findJobBtn.addEventListener("click", function (e) {
           
@@ -13,29 +12,3 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 });
 
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Sidebar.js loaded after DOM ready");
-
-  document.body.addEventListener("click", (e) => {
-    const target = e.target.closest("#logoutButton");
-    if (target) {
-      e.preventDefault();
-      console.log("Logout clicked");
-
-      Swal.fire({
-        title: 'Confirm Logout',
-        text: 'Are you sure you want to log out?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonText: 'Yes, logout',
-        cancelButtonText: 'Cancel',
-        reverseButtons: true
-      }).then((result) => {
-        if (result.isConfirmed) {
-          window.location.href = "/logout";
-        }
-      });
-    }
-  });
-});
