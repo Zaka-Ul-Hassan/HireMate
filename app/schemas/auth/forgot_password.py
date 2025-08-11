e031dca13c46976220ddf982bce2f84693311eed
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2025-08-11 21:51:05 +0500

    Add forgot_password schema

---
+++ app/schemas/auth/forgot_password.py
@@ -0,0 +1,18 @@
+# app\schemas\auth\forgot_password.py
+
+from pydantic import BaseModel,EmailStr
+
+class ForgotPasswordRequest(BaseModel):
+    email: EmailStr
+
+class ForgotPasswordResponse(BaseModel):
+    message: str
+
+
+class ResetPasswordRequest(BaseModel):
+    token: str
+    new_password: str
+
+
+class ResetPasswordResponse(BaseModel):
+    message: str
