e03155a160daa86c2b151f6facc1e2f843de31a7
Zaka-Ul-Hassan <zakaulhassan6717@gmail.com>
2026-01-28 19:35:28 +0500

    Fix Migration

---
+++ app/models/base/all_models.py
@@ -0,0 +1,6 @@
+# app\models\base\all_models
+from app.models.user.user import User
+from app.models.user.role import Role
+from app.models.user.user_role import UserRole
+from app.models.resume.resume_model import Resume
+from app.models.email.email_model import Email
--- init_db.py
+++ init_db.py
@@ -1,19 +1,43 @@
 # init_db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+import pyodbc
+from load_env import DB_DRIVER, DB_SERVER, DB_NAME, DB_TRUSTED_CONNECTION
+import app.models  # root package for models
 
-from app.models.base.model_base import Base
-from app.db import engine
+Base = declarative_base()
 
-from app.models.user.role  import Role
-from app.models.user.user_role import UserRole
-from app.models.email.email_model import Email
-from app.models.resume.resume_model import Resume
+from app.models.base import all_models
+
+# Build connection string
+driver = DB_DRIVER.replace(" ", "+")
+connection_string = (
+    f"mssql+pyodbc://@{DB_SERVER}/{DB_NAME}?driver={driver}&trusted_connection={DB_TRUSTED_CONNECTION.lower()}"
+)
+
+engine = create_engine(connection_string, echo=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-def create_table():
-    Base.metadata.create_all(bind=engine)
-    print(Base.metadata.tables.keys())
 
-    print("Table created successfully.")
+def get_connection():
+    
+    conn_str = (
+        f"DRIVER={{{DB_DRIVER}}};"
+        f"SERVER={DB_SERVER};"
+        f"DATABASE={DB_NAME};"
+        f"Trusted_Connection={DB_TRUSTED_CONNECTION};"
+    )
 
-if __name__ == "__main__":
-    create_table()  
+    connection = pyodbc.connect(conn_str)
+    try:
+        yield connection
+    finally:
+        connection.close()
